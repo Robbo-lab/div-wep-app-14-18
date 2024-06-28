b9b9d9d481a220a919d2135d0cf613deb6ab9c74
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2024-06-28 14:57:05 +0800

    feat: up to layout compoentn session 15

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>John's Next Appp</title>
+        <title>John's Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
